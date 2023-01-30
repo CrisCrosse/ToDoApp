b0a7a383d3dad609b7e36616d476a6d5b010ff3b
chris <chrisrossell1999@gmail.com>
2023-01-30 14:29:17 +0000

    Added Day 16 changes one variable for functions and a __main__ print function

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
     """returns a list of the todos created from the stored
     text file with a new item on each line"""
 
@@ -7,9 +10,14 @@ def get_todos(filepath="todos.txt"):
     return todos_local
 
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Creates a file or overwrites a file, writes a list item onto each line
     from the list input"""
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
     return
+
+
+if __name__ == "__main__":
+    print("my functions file is being run as the main file")
+    print(get_todos())
